f4de4dd31330096bdf7f1551c656c28ac2d5cb59
Aaron Leung <aaronleung@Aarons-MacBook-Air-3.local>
2025-02-27 08:49:05 -0800

    Class04

---
+++ Class04/class04.R
@@ -0,0 +1,14 @@
+#' ---
+#' title: "Lab Class04"
+#' author: "Aaron Leung (PID A17544470)"
+#' ---
+
+
+x <- 1:50
+plot(x, sin(x))
+
+# Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
+
+x <- c(6, 12, 4, 15)
+x != 6
